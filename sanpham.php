c65bcdbabc3a8465a0a548c5aad1d10f4f47fc00
khaduong <lekhaduong2002@gmail.com>
2025-02-17 13:05:26 +0700

    c

---
+++ sanpham.php
@@ -0,0 +1,94 @@
+<header>
+    <h1>Danh sách sản phẩm</h1>
+    <div class="search-bar">
+        <input type="text" placeholder="Nhập mã sản phẩm hoặc tên sản phẩm">
+        <select>
+            <option value="default">--Danh mục--</option>
+        </select>
+        <select>
+            <option value="default">--Nhà sản xuất--</option>
+        </select>
+        <button>Tìm kiếm</button>
+    </div>
+</header>
+<table>
+    <thead>
+        <tr>
+            <th>Tên sản phẩm</th>
+            <th>Mã sản phẩm</th>
+            <th>SL</th>
+            <th>Giá bán</th>
+            <th>Danh mục</th>
+            <th>Nhà sản xuất</th>
+            <th>Hành động</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Đường cát</td>
+            <td>SP00008</td>
+            <td>9</td>
+            <td>18,000</td>
+            <td>Đường</td>
+            <td>apple</td>
+            <td><button>Chỉnh sửa</button> <button>Xóa</button></td>
+        </tr>
+        <tr>
+            <td>Dòng phục nam</td>
+            <td>SP00007</td>
+            <td>50</td>
+            <td>120,000</td>
+            <td>Dòng phục nam</td>
+            <td>cenlia</td>
+            <td><button>Chỉnh sửa</button> <button>Xóa</button></td>
+        </tr>
+        <tr>
+            <td>Đường cát</td>
+            <td>SP00005</td>
+            <td>7</td>
+            <td>10,000</td>
+            <td>Đường</td>
+            <td>apple</td>
+            <td><button>Chỉnh sửa</button> <button>Xóa</button></td>
+        </tr>
+        <tr>
+            <td>Múi bọt</td>
+            <td>SP00004</td>
+            <td>29</td>
+            <td>7,000</td>
+            <td>Múi</td>
+            <td>cenlia</td>
+            <td><button>Chỉnh sửa</button> <button>Xóa</button></td>
+        </tr>
+        <tr>
+            <td>Đồng phục nam</td>
+            <td>SP00003</td>
+            <td>23</td>
+            <td>75,000</td>
+            <td>Đồng phục nam</td>
+            <td>cenlia</td>
+            <td><button>Chỉnh sửa</button> <button>Xóa</button></td>
+        </tr>
+        <tr>
+            <td>Áo nữ</td>
+            <td>SP00002</td>
+            <td>4</td>
+            <td>150,000</td>
+            <td>Áo</td>
+            <td>cenlia</td>
+            <td><button>Chỉnh sửa</button> <button>Xóa</button></td>
+        </tr>
+        <tr>
+            <td>Quần nam</td>
+            <td>SP00001</td>
+            <td>15</td>
+            <td>140,000</td>
+            <td>Quần</td>
+            <td>cenlia</td>
+            <td><button>Chỉnh sửa</button> <button>Xóa</button></td>
+        </tr>
+    </tbody>
+</table>
+<footer>
+    <p>SL sản phẩm/SL nhà sản xuất: 8/2</p>
+</footer>
